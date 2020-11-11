725e9438b87f242a15f35f5fa469190a6491389b
MatthewSissom <matthewesissom@gmail.com>
2020-11-10 19:16:01 -0500

    Stamina Bar
    finished first pass of stamina bar

---
+++ Assets/Stamina/Stamina.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamina : MonoBehaviour
+{
+
+    [Header("Game Feel")]
+    [SerializeField]
+    //how quickly the player regains stamina
+    float staminaPerSecond;
+    [SerializeField]
+    //how much of spent universal stamina is converted into special stamina
+    float conversionPercentage;
+
+    [Header("Experimental")]
+    //TEMP values for testing
+    [SerializeField]
+    bool debugClicks;
+    [SerializeField]
+    bool regenOverSpecial;
+    [SerializeField]
+    bool allowNegitiveStamina;
+
+    //all stamina values range from 0 - 100
+    public float universalStamina { get; private set; }
+    public float physicalStamina  { get; private set; }
+    public float magicStamina { get; private set; }
+
+    StaminaUI ui;
+
+    //called to perform a physical attack, returns false if
+    //the player is unable to perform the attack
+    public bool Attack(float cost, bool isPhysical)
+    {
+        //check required stamina and spend any available special stamina
+        if(isPhysical)
+        {
+            if (universalStamina + physicalStamina > cost ||
+                (allowNegitiveStamina && universalStamina > 0))
+            {
+                //adjust universal cost to reflect physical stamina spent on attack
+                physicalStamina -= cost;
+                if (physicalStamina < 0)
+                {
+                    cost = -physicalStamina;
+                    physicalStamina = 0;
+                }
+                else
+                {
+                    //universal cost is 0, can return early
+                    return true;
+                }
+            }
+            //not enough stamina, return
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (universalStamina + magicStamina > cost ||
+                (allowNegitiveStamina && universalStamina > 0))
+            {
+                //adjust universal cost to reflect magic stamina spent on attack
+                magicStamina -= cost;
+                if (magicStamina < 0)
+                {
+                    cost = -magicStamina;
+                    magicStamina = 0;
+                }
+                else
+                {
+                    //universal cost is 0, can return early
+                    return true;
+                }
+            }
+            //not enough stamina, return
+            else
+            {
+                return false;
+            }
+        }
+
+        //any remaining cost after spending special stamina is taken from universal stamina
+        //and refunded to the type oppisite the type of the attack
+        universalStamina -= cost;
+        if (!allowNegitiveStamina || universalStamina > cost)
+        {
+            if (isPhysical)
+            {
+                magicStamina += conversionPercentage * cost;
+            }
+            else
+            {
+                physicalStamina += conversionPercentage * cost;
+            }
+        }
+        //special calculations for negitive stamina
+        else
+        {
+            if (isPhysical)
+            {
+                magicStamina += conversionPercentage * (cost + universalStamina);
+            }
+            else
+            {
+                physicalStamina += conversionPercentage * (cost + universalStamina);
+            }
+        }
+
+
+        return true;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //stamina regen
+        if(universalStamina < 100)
+        {
+            universalStamina = Mathf.Min(100, Time.deltaTime * staminaPerSecond + universalStamina);
+            if(regenOverSpecial && universalStamina > 0)
+            {
+                if (physicalStamina > 0)
+                {
+                    physicalStamina -= Time.deltaTime * staminaPerSecond;
+                }
+                else if (magicStamina > 0)
+                {
+                    magicStamina -= Time.deltaTime * staminaPerSecond;
+                }
+            }
+        }
+        //cap total stamina at 100
+        physicalStamina = Mathf.Min(100 - universalStamina, physicalStamina);
+        magicStamina = Mathf.Min(100 - magicStamina, magicStamina);
+
+        ui.UpdateValues(universalStamina / 100, physicalStamina / 100, magicStamina / 100);
+
+        if (debugClicks)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack(20, true);
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                Attack(20, false);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        universalStamina = 100;
+        magicStamina = 0;
+        physicalStamina = 0;
+        ui = FindObjectOfType<StaminaUI>();
+    }
+}
---
+++ Assets/Stamina/StaminaUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject uniBar;
+    private RectTransform uniTrans;
+    private Image uniImage;
+    [SerializeField]
+    private GameObject magicBar;
+    private Image magicImage;
+    private RectTransform magicTrans;
+    [SerializeField]
+    private GameObject physBar;
+    private Image physImage;
+    private RectTransform physTrans;
+
+    public float offset;
+
+    public void UpdateValues(float uni, float phys, float magic)
+    {
+        uni = uni < 0 ? 0 : uni;
+        SetBarPosition(uniImage, uni);
+        SetBarPosition(magicImage, magic, magicTrans, uni);
+        SetBarPosition(physImage, phys, physTrans , uni);
+    }
+
+    private void SetBarPosition(Image img, float percentage, RectTransform trans = null, float startPercentage = 0)
+    {
+        img.fillAmount = percentage;
+        if(trans)
+        {
+            //763 is the width of the bar in pixels
+            trans.anchoredPosition = new Vector3(offset* startPercentage,0,0) ;
+        }
+    }
+
+    private void Start()
+    {
+        uniImage = uniBar.GetComponent<Image>();
+        physImage = physBar.GetComponent<Image>();
+        magicImage = magicBar.GetComponent<Image>();
+        uniTrans = uniBar.GetComponent<RectTransform>();
+        physTrans = physBar.GetComponent<RectTransform>();
+        magicTrans = magicBar.GetComponent<RectTransform>();
+    }
+}
