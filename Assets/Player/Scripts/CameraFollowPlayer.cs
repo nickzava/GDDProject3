62f2f8d02747b4ce55749990b7e52d9958461d8b
liamgu3 <43424244+liamgu3@users.noreply.github.com>
2020-11-19 21:01:43 -0500

    camera following working, wall collision mostly working

---
+++ Assets/Player/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+	public GameObject player;
+	public float camDistance = -10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -camDistance);
+    }
+}
