1330fb88b31372bf6e32f0470da3c5517b229fd2
oracra <jetfire519@gmail.com>
2020-11-20 16:01:52 -0500

    Added Fireball Attack class

---
+++ Assets/Attacks/FireballAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballAttack : BasicAttack
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected new void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<EnemyBase>() != null)
+        {
+            other.attachedRigidbody.AddForce(Quaternion.Inverse(other.transform.rotation) * new Vector3(1, 1, 0));
+        }
+    }
+}
