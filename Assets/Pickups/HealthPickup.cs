3d8c8ca1129725bca5081d9edc70fe7e7ffebfae
liamgu3 <43424244+liamgu3@users.noreply.github.com>
2020-11-23 18:17:41 -0500

    fixed tutorial text and added pickups

---
+++ Assets/Pickups/HealthPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : BasePickup
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		base.Start();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		base.Update();
+	}
+
+	protected override void OnTriggerEnter(Collider other)
+	{
+		if (player.GetComponent<Player>().Health < 3)
+		{
+			player.GetComponent<Player>().Health += 1;
+		}
+	}
+}
