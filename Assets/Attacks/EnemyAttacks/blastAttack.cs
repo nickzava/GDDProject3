0d2895d10e9f0b503b3da8f826989ff352ae25af
Will Kracke <wjk4218@rit.edu>
2020-11-30 01:00:58 -0500

    Enemy Prefabs and Ranged attacks

---
+++ Assets/Attacks/EnemyAttacks/blastAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blastAttack : Projectile
+{
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        //check the type of collision before exploding
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("wall"))
+        {
+            Hit(collision.gameObject.GetComponent<EnemyBase>());
+        }
+    }
+
+    private void Hit(EnemyBase directHitTarget = null)
+    {
+        //Do bonus damage to direct hits
+        if (directHitTarget)
+        {
+            //directHitTakesDamage
+
+            directHitTarget.health -= 1;
+
+        }
+
+        Destroy(gameObject);
+    }
+}
