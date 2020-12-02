dc3c2528c8f294faac266fe2d82871b719d16ad4
liamgu3 <43424244+liamgu3@users.noreply.github.com>
2020-12-02 14:54:19 -0600

    added level changing functionality

---
+++ Assets/Menus/NextLevel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+	public GameObject[] levels;
+	public static int currentLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentLevel = 0;
+		//for(int i = 1; i < levels.Length; i++)	//di
+		//{
+		//	if (i != currentLevel)
+		//	{
+		//		levels[i].SetActive(false);
+		//	}
+		//}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			SetActiveAllChildren(levels[currentLevel].transform, false);
+			currentLevel++;
+			SetActiveAllChildren(levels[currentLevel].transform, true);
+			Camera.main.GetComponent<CameraFollowPlayer>().player = GameObject.FindGameObjectWithTag("Player");
+		}
+	}
+
+	private void SetActiveAllChildren(Transform transform, bool value)
+	{
+		transform.gameObject.SetActive(value);
+		foreach (Transform child in transform)
+		{
+			child.gameObject.SetActive(value);
+
+			SetActiveAllChildren(child, value);
+		}
+	}
+}
