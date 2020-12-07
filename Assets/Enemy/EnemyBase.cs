152078e87e4175358627c947a6016bf087763374
Will Kracke <wjk4218@rit.edu>
2020-12-07 17:32:55 -0500

    Enemies Raycast Better

--- Assets/Enemy/EnemyBase.cs
+++ Assets/Enemy/EnemyBase.cs
@@ -208,7 +208,7 @@ public abstract class EnemyBase : MonoBehaviour
             RaycastHit hitInfo;
             int rayLength = 10;
 
-            if (Physics.Raycast(gameObject.transform.position, (goal.transform.position - gameObject.transform.position), out hitInfo, rayLength))
+            if (Physics.Raycast(gameObject.transform.position, (goal.transform.position - gameObject.transform.position), out hitInfo, rayLength, -5, QueryTriggerInteraction.Ignore))
             {
                 if (hitInfo.collider.gameObject.tag == "Player")
                 {
